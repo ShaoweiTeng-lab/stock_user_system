ff68fef87ddae480a593fd8b2af27282455499cc
Jason Teng <sdonline13@gmail.com>
2025-01-12 09:13:59 +0800

    init

---
+++ src/main/java/com/example/stockusersystem/StockUserSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockusersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockUserSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockUserSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stockusersystem/StockUserSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockusersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockUserSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
